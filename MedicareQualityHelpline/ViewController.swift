c481bd9b452a56fa39997a7d842617a562d9da0c
bradcollins <bradthedev@gmail.com>
2018-01-05 16:17:47 -0500

    Initial Commit

---
+++ MedicareQualityHelpline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedicareQualityHelpline
+//
+//  Created by Brad Collins on 1/5/18.
+//  Copyright © 2018 Brad Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
